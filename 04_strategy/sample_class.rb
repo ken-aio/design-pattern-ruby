dc076ab28e5aba39f9424b7ac21a08c8843049c5
Suguru Akiho <s-akiho@unext.jp>
2016-07-29 13:39:02 +0900

    Add Strategy pattern

---
+++ 04_strategy/sample_class.rb
@@ -0,0 +1,46 @@
+class Report
+  attr_reader :title, :text
+  attr_accessor :formatter
+
+  def initialize(formatter)
+    @title = '月次報告'
+    @text = [ '順調', '最高' ]
+    @formatter = formatter
+  end
+
+  def output_report
+    @formatter.output_report(self)
+  end
+end
+
+class HTMLFormatter
+  def output_report(context)
+    puts('<html>')
+    puts('  <head>')
+    puts("    <title>#{context.title}</title>")
+    puts('  </head>')
+    puts('  <body>')
+    context.text.each do |line|
+      puts("      <p>#{line}</p>")
+    end
+    puts('  </body>')
+    puts('</html>')
+  end
+end
+
+class PlainTextFormatter
+  def output_report(context)
+    puts("*** #{context.title} ***")
+    context.text.each do |line|
+      puts(line)
+    end
+  end
+end
+
+report = Report.new(HTMLFormatter.new)
+report.output_report
+
+puts('-------')
+
+report = Report.new(PlainTextFormatter.new)
+report.output_report
